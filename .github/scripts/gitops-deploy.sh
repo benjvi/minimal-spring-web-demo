1e381de9425c93ffc9ed2fa42734b7bd35107bb8
Ben Vickers <bvickers@vmware.com>
2021-05-04 16:39:50 +0200

    more fixing app name

--- .github/scripts/gitops-deploy.sh
+++ .github/scripts/gitops-deploy.sh
@@ -8,21 +8,23 @@ cd /
 GIT_SSH_COMMAND='ssh -i "$KEYFILE" -o IdentitiesOnly=yes -o StrictHostKeyChecking=no' git clone git@github.com:benjvi/apps-gitops.git
 cd -
 
+APP_NAME="spring-app"
+IMAGE_NAME="minimal-spring-web-demo"
 IMG_VERSION="$(git rev-parse --short=8 HEAD)"
-# TODO: add kshard in here
 echo "$IMG_VERSION"
 
 cd k8s
 # need to set the reference to the published image in the manifest, note this is specific to:
 # (1) our use of kustomize manifests in this repo (2) use of TBS
-kustomize edit set image "index.docker.io/benjvi/minimal-spring-web-demo=index.docker.io/benjvi/minimal-spring-web-demo@${IMG_VERSION}"
+kustomize edit set image "index.docker.io/benjvi/$IMAGE_NAME=index.docker.io/benjvi/${IMAGE_NAME}@${IMG_VERSION}"
 # rendering of the manifests to deal with *app specific customizations*, note this is specific to our choice of packaging
-kustomize build . > /apps-gitops/nonprod-cluster/angular-app/ci-package.yml
+# TODO: add kshard in here
+kustomize build . > "/apps-gitops/nonprod-cluster/${APP_NAME}/ci-package.yml"
 cd -
 
 # add empty kustomize config to nonprod gitops fodler
 # reasons to use kustomize in the gitops repo is not yet clear in this simple example, but will be useful when envs have different convifgs
-cd /apps-gitops/nonprod-cluster/angular-app
+cd "/apps-gitops/nonprod-cluster/${APP_NAME}"
 kustomize create || true; kustomize edit add resource ci-package.yml
 cd -
 
